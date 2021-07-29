3b340aa7809c41196561b8108da4d8821231ea77
gumball09 <vivianpham0904@gmail.com>
2021-07-29 02:07:19 -0400

    Add endpoints for healthcheck & dummy endpoint

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// MAKE THE SURE THE APPLICATION IS UP AND RUNNING AFTER DEPLOYMENT
+// Health check
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
