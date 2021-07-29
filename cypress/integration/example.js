8a7d036a2841664be55fae2a15c8271f53878b52
gumball09 <vivianpham0904@gmail.com>
2021-07-29 01:00:12 -0400

    E2E testing

--- cypress/integration/example.js
+++ cypress/integration/example.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-undef */
 describe('Pokedex app', function () {
-  it('Front app can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('Front app can be opened', function () {
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('Navigate from main page to particular pokemon', function () {
+    cy.contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
   })
 })
